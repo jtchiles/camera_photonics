c799ca8706f711303ab6b0c7480fb9ce6b4c5c36
jtchiles <jeffrey.chiles@nist.gov>
2018-07-09 12:42:22 -0600

    overhaul of code
    -added config file import "config.ini" in local directory
    
    -added background subtraction via "use_darkfield" option and associated filename
    
    -added ability for the user to only look for ports within an ROI with the option "use_valid_box".
    
    -now automatically detects the correct dimension to sort the port data along based on the variance of the port location data.  Does not sort by power anymore.
    
    -cleaned up unused old code bits
    
    -removed individual subplots in favor of a unified image with all the integration windows and normalized powers overlaid at once.

--- f_camera_photonics.py
+++ f_camera_photonics.py
@@ -12,34 +12,71 @@ import math
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.text
-#import os
-#os.chdir('C:\\folder_with_image_file')
+import configparser as cp
+import os
 
 varargin = 0
 
 def f_camera_photonics(filename, varargin = 0):
-#    filename = "smb_tminput_shortsp_shortrt_tmout.tiff"
+
+    #first, get the os-independent directories and filenames put together
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    prefix=os.getcwd()
+    Config = cp.ConfigParser()
+    configfile=os.path.realpath(prefix+"\\" +"config.ini")
+    print(str(configfile))
+    Config.read(configfile)
+    filename = os.path.realpath(prefix+"\\" + filename)
+
+    #collect the config file's data 
+    col=Config.getint("camera_attributes","horizontal_resolution")
+    row=Config.getint("camera_attributes","vertical_resolution")
+    bit_depth_per_pixel=Config.getint("camera_attributes","bit_depth_per_pixel")
+    use_darkfield=Config.getboolean("camera_attributes","use_darkfield")
+    use_brightfield=Config.getboolean("camera_attributes","use_brightfield")
+    darkfield_filename=Config.get("camera_attributes","darkfield_filename")
+    brightfield_filename=Config.get("camera_attributes","brightfield_filename")
+
+    use_blanking_box=Config.getboolean("signal_processing","use_blanking_box")
+    use_valid_box=Config.getboolean("signal_processing","use_valid_box")
+    box_width=Config.getint("signal_processing","box_width")
+    max_box_width=Config.getint("signal_processing","max_box_width")
+    min_residual=Config.getfloat("signal_processing","min_residual")
+    pixel_increment=Config.getint("signal_processing","pixel_increment")
+    default_nports=Config.getint("signal_processing","default_nports")
+    saturation_level_fraction=Config.getfloat("signal_processing","saturation_level_fraction")
+
     if varargin == 0:
-        nports = 5
+        nports = default_nports
         box = []
         x_set = []
         y_set = []
-        radius_set = []
+        box_width_set = []
     else:
         box = varargin[0]
         x_set = box[:,0]
         y_set = box[:,1]
-        radius_set = box[:,3]
+        box_width_set = box[:,3]
         nports = len(x_set)
 
-
+    #null out pixels within an ROI
     def make_pixels_black(img, dimensions, value=0):
         if (len(dimensions) == 4):
             for x in range(dimensions[1], dimensions[1]+dimensions[3]):
                 for y in range(dimensions[0],dimensions[0]+dimensions[2]):
-                    img[x,y] = value;
+                    img[x,y] = value
         return img
 
+    #replace the pixels in an ROI of one array with those of another array
+    def swap_pixel_data(img_orig, img_new, dimensions):
+        img_temp=img_orig
+        if (len(dimensions) == 4):
+            for x in range(dimensions[1], dimensions[1]+dimensions[3]):
+                for y in range(dimensions[0],dimensions[0]+dimensions[2]):
+                    img_temp[x,y] = img_new[x,y]
+        return img_temp
+
+    #calculate mean value over an ROI
     def calculate_pixel_mean(img, dimensions):
         pixel_count = 0
         pixel_sum = 0
@@ -51,97 +88,83 @@ def f_camera_photonics(filename, varargin = 0):
         pixel_avg = pixel_sum / pixel_count
         return pixel_avg
 
-    #open file as array of uint8 for veiwing and selecting
+    #open file as array of uint8 for viewing and selecting
     img = cv2.imread(filename,0)
     img_array = np.array(img)
-#    plt.imshow(img_array)
+
+    if(use_darkfield is True):
+        darkfield_filename=os.path.realpath(prefix+"\\" +darkfield_filename)
+        img_darkfield=cv2.imread(darkfield_filename,-1)
+        
+
     #open file as full 16 bit tiff image
     img2 = cv2.imread(filename,-1)
-    
     max_img8bit = np.max(img)
     scaled_img = img*max_img8bit
-
+    
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
 #   IF THE IMAGE SHOWS ALL BLACK, UNCOMMENT THIS CODE AND COMMENT THE BELOW SECTION
     
-#    img_8bit = scaled_img - np.min(scaled_img)
-#
-#    img_8bit = np.log(img_8bit)
-#    img_8bit = 255 - img_8bit / (np.max(img_8bit))*255
-#    img_8bit = np.array(img_8bit, dtype = np.uint8)
-#    img_array = img_8bit
+    img_8bit = scaled_img - np.min(scaled_img)
+
+    img_8bit = np.log10(img_8bit)
+    img_8bit = 255 - img_8bit / (np.max(img_8bit))*255
+    img_8bit = np.array(img_8bit, dtype = np.uint8)
+    img_array = img_8bit
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     # ONLY ONE OF THESE TWO SECTIONS SHOULD BE UNCOMMENTED AT A TIME
 
-    img_8bit = np.log(img)
-    img_8bit = img_8bit - np.min(img_8bit)
-    img_8bit = 255 - img_8bit / (np.max(img_8bit))*255
-    img_8bit = np.array(img_8bit, dtype = np.uint8)
-    img_array = img_8bit
-
+    # img_8bit = np.log(img)
+    # img_8bit = img_8bit - np.min(img_8bit)
+    # img_8bit = 255 - img_8bit / (np.max(img_8bit))*255
+    # img_8bit = np.array(img_8bit, dtype = np.uint8)
+    # img_array = img_8bit
+    # plt.imshow(img_array)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    ## WARNING: DO NOT ATTEMPT TO SHOW THE 16 bit file because it will probably crash python
-    print("\n\nSelect a region of pixels which you want to be blacked out. Every pixel inside this box will be set to black. When you see the white box hit enter or space.")
-    r = cv2.selectROI(windowName="Black out Region Selector", img=img_array)
-    img_array = make_pixels_black(img_array, r,255)
-    img2 = make_pixels_black(img2, r)
-    cv2.destroyWindow("Black out Region Selector")
+    #If the user chooses to use a "blanking box", open an ROI selector and null out everything inside the ROI.
+    if use_blanking_box is True:
+        print("\n\nSelect a region of pixels which you want to be zeroed out. Every pixel inside this box will be set to black. When you see the white box hit enter or space.")
+        r = cv2.selectROI(windowName="Black out Region Selector", img=img_array)
+        img_array = make_pixels_black(img_array, r,255)
+        img2 = make_pixels_black(img2, r)
+        cv2.destroyWindow("Black out region selector")
 
-    print("\n\nSelect a region of pixels which will be used to calculate the mean value of the image. This region should not contain the black region above, or any of the bright spots on the image. When you see the white box, hit enter or space.")
-    mean_r = cv2.selectROI(windowName="Mean Pixel Value Selector", img=img_array)
-    mean_value = calculate_pixel_mean(img_array, mean_r)
-    mean_value2 = calculate_pixel_mean(img2, mean_r)
+    #If the user chooses to use a "valid box", open an ROI selector and null out everything outside the ROI.
+    if use_valid_box is True:
+        print("\n\nSelect a valid region of pixels to look for all output ports.  Everywhere else will be zeroed out.")
+        r = cv2.selectROI(windowName="Valid region selector", img=img_array)
+        img_array = make_pixels_black(img_array, r,255)
+        img2_temp=img2
+        img2_temp=img2_temp*0
+        img2 = swap_pixel_data(img2_temp, img2,r)
+        cv2.destroyWindow("Valid region selector")
 
-    #print(mean_value)
-    #print(mean_value2)
-    cv2.destroyWindow("Mean Pixel Value Selector")
-    #cv2.imshow('image',img)
 
-    #cv2.waitKey(0)
+    #subtract darkfield (background) image from main data.
+    img2=np.subtract(img2.astype(float),img_darkfield.astype(float))   
+    img2[img2 < 0] = 0 # set all negative values to 0
+    print("The maximum value in the image after darkfield correction is: "+str(np.amax(img2)) +" (out of a camera limit of " +str(math.pow(2,bit_depth_per_pixel)-1)+")")
 
-    cv2.destroyAllWindows()
 
     #/////////////////////////////////////////////////////////////////////////////////////////////
     # End User Interface portion and begin calculations
-    # Note: the pixel mean calculation was already performed in the function calculate_pixel_mean
-    # Note: the black box was already created as well
+    # At this point, any desired background and ROI corrections have been completed.
     #/////////////////////////////////////////////////////////////////////////////////////////////
 
-    #%%
+    
+    saturation_level=math.pow(2,bit_depth_per_pixel)*saturation_level_fraction #calculate the threshold for the saturation condition
+    maxval = np.amax(img2) #find max value in the entire image
 
-    # Set Parameters
-    radius = 3 #radius of spot size for each port
-    row = 512
-    col = 640 # expected image size
-    #bright_pixel_threshold = 5000 #16 bit minimum value of a "bright" pixel
-
-    min_residual = 1.03
-    if(type(img2[0,0]) == np.uint16):
-        saturation_level = math.pow(2,16)-3000
-    elif(type(img2[0,0]) == np.uint8):
-        saturation_level = math.pow(2,8)-30
-    elif(type(img2[0,0]) == np.uint32):
-        saturation_level = math.pow(2,32)-10000000
-    pixel_increment = 3
-    #bloom = 1
-    rmax = 10
-
-    img2 = np.subtract(img2, mean_value2) #subtract mean value from entire image
-    maxval = np.max(img2) #find max value
-    img2[img2 < 0] = 0 # set all negative values to 0
-
+    #check if saturation has occurred
     if(maxval >= saturation_level):
         print("## ERROR ##")
         print("Image Saturated!")
         sys.exit()
-
-    # Lines 57-63 of matlab file f_camera_photonics.m was ignored here
-
 
     # processing to find the gratings/ports
      # convolution-like operation scanning around the image to find power
@@ -149,26 +172,24 @@ def f_camera_photonics(filename, varargin = 0):
     figures = []
 
     if(len(box) <= 0): #if box exists
-        for i in range(radius*2+1, row-radius*2-pixel_increment, pixel_increment): #step by pixel increment
-            for j in range(radius*2+1, col-radius*2-pixel_increment, pixel_increment):
-                subregion = img2[i-radius:i+radius, j-radius:j+radius]
+        for i in range(box_width*2+1, row-box_width*2-pixel_increment, pixel_increment): #step by pixel increment
+            for j in range(box_width*2+1, col-box_width*2-pixel_increment, pixel_increment):
+                subregion = img2[i-box_width:i+box_width, j-box_width:j+box_width]
                 P = np.sum(subregion)
                 P_window.append([P, i, j])
 
         P_window = np.array(P_window)
         M,I = img2.max(0),img2.argmax(0)
-        #m = np.max(P_window)
-        #ind = np.argmax(P_window)
 
         #More Parameters
         P_ports = np.array([[]])
         prev_x = []
         prev_y = []
-        prev_radius = []
+        prev_box_width = []
         near_pixel = 0
         fig_count = 1
-        #find the top nports candidates based on power
 
+        #find the top nports candidates based on power
         while P_ports.shape[0] < nports or P_ports.shape[1] == 0:
             M,I = P_window.max(0),P_window.argmax(0)
             I2 = [P_window[I[0],1],P_window[I[0],2]]
@@ -177,36 +198,34 @@ def f_camera_photonics(filename, varargin = 0):
 
             for i in range(0, len(prev_x)):
                 #TODO fix line 112
-                if(abs(prev_x[i]-x) < prev_radius[i]*2) and (abs(prev_y[i] -y) < prev_radius[i]*2):
+                #see if the current candidate is too close to the last peak location
+                if(abs(prev_x[i]-x) < prev_box_width[i]*3) and (abs(prev_y[i] -y) < prev_box_width[i]*3):
                     P_window[I[0],0] = 0 # null out anything close to the previous peak
                     near_pixel = 1
 
 
             if (near_pixel == 0):
-                subregion = img2[int(x-radius):int(x+radius), int(y-radius):int(y+radius)]
+                subregion = img2[int(x-box_width):int(x+box_width), int(y-box_width):int(y+box_width)]
                 power_prev = np.sum(subregion)
                 power_current = power_prev
-                r = radius
-                residual=2;
-                while((residual > min_residual) and (r < rmax)):
+                r = box_width
+                residual=2
+                while((residual > min_residual) and (r < max_box_width)):
                     r = r+1
                     subregion = img2[int(x-r):int(x+r), int(y-r):int(y+r)]
                     power_current = np.sum(subregion)
-                    residual = power_current/power_prev
+                    if power_prev!=0:
+                        residual = power_current/power_prev
+                    else:
+                        residual=0
                     power_prev = power_current
 
-                plt.figure(fig_count)
-                plt.imshow(np.array(subregion))
-                plt.title("Spot at X= " + str(y) + ", Y= " + str(x)) ## somehow it seems x and y are switched here...
-                plt.colorbar()
-                plt.show()
-                figures.append(fig_count)
                 fig_count += 1
 
 
                 prev_x.append(x)
                 prev_y.append(y)
-                prev_radius.append(r)
+                prev_box_width.append(r)
 
                 if(P_ports.size > 0):
                     P_ports = np.concatenate([P_ports, P_window[int(I[0])].reshape(1,3)])
@@ -214,68 +233,87 @@ def f_camera_photonics(filename, varargin = 0):
                     P_ports = P_window[int(I[0])].reshape(1,3)
             near_pixel = 0
 
-        # now we have radius, x and y values, calculate the actaul powers
+        # now we have box_width, x and y values, calculate the actual powers from the real data
         P = []
         P_norm = []
         for i in range(0, nports):
             x = P_ports[i, 1]
             y = P_ports[i, 2]
-            this_radius = prev_radius[i]
-            #dim2 = np.array([y/col-1*this_radius/col, (row-x)/row-1*this_radius/row, 2*this_radius/col, 2*this_radius/row])
-            #dim3 = np.array([y/col-0.75*radius/col, (row-x)/row-0.75*radius/row, 2*radius/col, 2*radius/row])
-            subregion = img2[int(x)-this_radius:int(x)+this_radius, int(y)-this_radius:int(y)+this_radius]
+            this_box_width = prev_box_width[i]
+            subregion = img2[int(x)-this_box_width:int(x)+this_box_width, int(y)-this_box_width:int(y)+this_box_width]
             P.append(np.sum(subregion))
-            P_norm.append(P[i]/P[0])
+            P_norm.append(P[i])
 
             P_ports[i,0] = P[i]
-
-        for i, x in enumerate(figures):
-            #add power to figures
-            plt.figure(x)
-            plt.annotate("Normalized Power: " + str(P_norm[i]), (0,0.05), xycoords ='axes fraction', color="white")
-
-
-        radius = prev_radius
-    #    P_ports = np.sort(P_ports)
+        box_width = prev_box_width
         x_vec=P_ports[:,1]
         y_vec=P_ports[:,2]
     else:
         x_vec = x_set
         y_vec = y_set
-        radius = radius_set
+        box_width = box_width_set
         for i in range(0, nports):
-            this_radius = radius[i]
+            this_box_width = box_width[i]
             x = x_vec[i]
             y = y_vec[i]
-            subregion = img2[x-this_radius:x+this_radius, y-this_radius:y+this_radius]
+            subregion = img2[x-this_box_width:x+this_box_width, y-this_box_width:y+this_box_width]
             P[i] = np.sum(subregion)
         P_ports = np.array([P.transpose(), x_vec, y_vec])
 
+    xvar=np.var(P_ports[:,1])
+    yvar=np.var(P_ports[:,2])
+    if xvar>yvar:
+        P_ports=P_ports[P_ports[:,1].argsort()]
+        print("Detected that the ports should be sorted along the row index.  Proceeding with this assumption.")
+    else:
+        P_ports=P_ports[P_ports[:,2].argsort()]
+        print("Detected that the ports should be sorted along the column index.  Proceeding with this assumption.")
+    P_norm=P_ports[:,0]/np.amax(P_ports[:,0])
 
 
+    img2_scaled=img2/(math.pow(2,bit_depth_per_pixel)/16)
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    
+    fontScale              = 0.3
+    fontColor              = (1,1,1)
+    lineType               = 1
+    for i in range(0, nports): 
+        #draw a rectangle surrounding each port to represent the integration window.
+        cv2.rectangle(img2_scaled, (int(P_ports[i,2]-box_width[i]), int(P_ports[i,1]-box_width[i])), (int(P_ports[i,2] + box_width[i]), int(P_ports[i,1]+box_width[i])), (32,32,32), 1)
+        
+        annotation = "(#"+ str(i+1) +", " + "P: " +"{:.2f}".format(P_norm[i])+")"
+        #if you want to include the row and column indices, tack this on to the annotation: +", " + str(P_ports[i,2]) + ", " + str(P_ports[i,1]) + 
+        print(annotation)
 
-    for i in range(0, nports):
-        cv2.rectangle(img_array, (int(P_ports[i,2]-radius[i]), int(P_ports[i,1]-radius[i])), (int(P_ports[i,2] + radius[i]), int(P_ports[i,1]+radius[i])), (0,0,0), 2)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        annotation = "(" + str(P_ports[i,2]) + ", " + str(P_ports[i,1]) + ")"
-        cv2.putText(img_array,annotation,(int(P_ports[i,2]-4*radius[i]),int(P_ports[i,1]-2*radius[i])), font, 0.3,(0,0,0),1,cv2.LINE_AA)
-    cv2.imshow('Spot Locations Image',img_array)
-#    plt.figure()
-#    plt.imshow(img_array)
-#    plt.title("Location of Selected Spots")
+        #put the annotation near but slightly offset from the port location
+        location = (int(P_ports[i,2]+box_width[i]),int(P_ports[i,1]-0.3*box_width[i]))
+
+        cv2.putText(img2_scaled,
+            annotation, 
+            location, 
+            font, 
+            fontScale,
+            fontColor,
+            lineType)
+    #scale it up for readability
+    big=cv2.resize(img2_scaled, (0,0), fx=3, fy=3) 
+    cv2.imshow("img2",big)
 
 
-    pout = {"Total Power": P_ports[:,0], "Normalized Power":P_norm, "x":P_ports[:,1], "y":P_ports[:,2], "radius":radius}
-#    print("\n\nPress 0 to close the image and return the function")
-#    cv2.waitKey(0)
-#    cv2.destroyWindow("Spot Locations Image")
+    #cv2.waitKey(0)
+
+    
+    pout = {"Total Power": P_ports[:,0], "Normalized Power":P_norm, "x":P_ports[:,1], "y":P_ports[:,2], "box_width":box_width}
+    print("\n\nPress 0 to close the image and return the function")
+    cv2.waitKey(0)
+    cv2.destroyWindow("img2")
     return pout
 
 
 
 if(__name__ == "__main__"):
 #    filename = "grating1_TE_1310nm_300.tiff"
-    filename = "smb_tminput_shortsp_shortrt_tmout.tiff"
+    filename = "first_look-nolamp.tif"
 
     pout = f_camera_photonics(filename, varargin = 0)
     print(pout)

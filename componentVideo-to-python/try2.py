6f1f810b847d21ab29f0b8a48432476bc7a826d8
Alex Tait <alexander.tait@nist.gov>
2018-10-17 14:24:26 -0600

    modularized the image capture

--- componentVideo-to-python/try2.py
+++ componentVideo-to-python/try2.py
@@ -1,36 +1,55 @@
 # Trying to grab images out of cv2 with the USB cam
 import cv2
+import os
+from contextlib import contextmanager
 
-# Camera 0 is the integrated web cam on my netbook
 camera_port = 0
- 
-#Number of frames to throw away while the camera adjusts to light levels
-ramp_frames = 30
- 
-# Now we can initialize the camera capture object with the cv2.VideoCapture class.
-# All it needs is the index to a camera port.
-camera = cv2.VideoCapture(camera_port)
- 
-# Captures a single image from the camera and returns it in PIL format
-def get_image():
-	# read is the easiest way to get a full image out of a VideoCapture object.
-	retval, im = camera.read()
-	return im
- 
-# Ramp the camera - these frames will be discarded and are only used to allow v4l2
-# to adjust light levels, if necessary
-for i in range(ramp_frames):
-	temp = get_image()
-print("Taking image...")
-# Take the actual image we want to keep
-camera_capture = get_image()
-del(camera)
 
-#file = "/home/codeplasma/test_image.png"
-# A nice feature of the imwrite method is that it will automatically choose the
-# correct format based on the file extension you provide. Convenient!
-#cv2.imwrite('.', camera_capture)
-cv2.imshow('img', camera_capture)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('image.png', camera_capture)
+@contextmanager
+def open_camera():
+    camera = cv2.VideoCapture(camera_port)
+    yield camera
+    del(camera)
+
+
+## Low level conditioning
+# Number of frames to throw away while the camera adjusts to light levels
+ramp_frames = 30
+
+def get_frames(nframes=1):
+    with open_camera() as camera:
+        for i in range(ramp_frames):
+            camera.read()
+        frame_list = []
+        for i in range(nframes):
+            _, img = camera.read()
+            frame_list.append(img)
+    return frame_list
+
+
+def single_shot():
+    return get_frames(1)[0]
+
+
+def average(nframes=2):
+    stack = np.array(get_frames(nframes))
+    return np.mean(stack, axis=2)
+
+
+def show(cvimg):
+    print('Press any key to close the display window')
+    cv2.imshow('img', camera_capture)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def save(cvimg, filename):
+    ''' You can specify extension or not. Default in .png '''
+    if '.' not in filename:
+        filename += '.png'
+    cv2.imwrite(filename, camera_capture)
+
+
+if __name__ == '__main__':
+    cvimg = get_frames()[0]
+    show(cvimg)
